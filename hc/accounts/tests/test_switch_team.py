730cf7b6751136a7bd6525e78bacf83edbe41e44
white_viel <brian.rotich@andela.com>
2016-11-22 23:30:45 +0300

    [chore #133846505] add test for profile

--- hc/accounts/tests/test_switch_team.py
+++ hc/accounts/tests/test_switch_team.py
@@ -1,9 +1,11 @@
-from hc.test import BaseTestCase
+from django.http import HttpResponseForbidden
+from django.http import HttpResponseRedirect
+
 from hc.api.models import Check
+from hc.test import BaseTestCase
 
 
 class SwitchTeamTestCase(BaseTestCase):
-
     def test_it_switches(self):
         c = Check(user=self.alice, name="This belongs to Alice")
         c.save()
@@ -13,15 +15,15 @@ class SwitchTeamTestCase(BaseTestCase):
         url = "/accounts/switch_team/%s/" % self.alice.username
         r = self.client.get(url, follow=True)
 
-        ### Assert the contents of r
-
+        self.assertContains(r, 'This belongs to Alice')
 
     def test_it_checks_team_membership(self):
         self.client.login(username="charlie@example.org", password="password")
 
         url = "/accounts/switch_team/%s/" % self.alice.username
         r = self.client.get(url)
-        ### Assert the expected error code
+
+        self.assertEqual(r.status_code, HttpResponseForbidden.status_code)
 
     def test_it_switches_to_own_team(self):
         self.client.login(username="alice@example.org", password="password")
