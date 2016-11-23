110006dd25da69904812510f92eb5021e4a5250c
Gacheru <evans.gacheru@andela.com>
2016-11-23 11:04:46 +0300

    [chore#133846507 Added function that validates priority test]

--- hc/front/tests/test_add_pushbover.py
+++ hc/front/tests/test_add_pushbover.py
@@ -37,4 +37,14 @@ class AddPushoverTestCase(BaseTestCase):
         r = self.client.get("/integrations/add_pushover/?%s" % params)
         assert r.status_code == 403
 
-    ### Test that pushover validates priority
+        ### Test that pushover validates priority
+    def test_it_validates_priority(self):
+        self.client.login(username="alice@example.org", password="password")
+
+        session = self.client.session
+        session["po_nonce"] = "n"
+        session.save()
+
+        params = "pushover_user_key=a&nonce=INVALID&prio=abc"
+        r = self.client.get("/integrations/add_pushover/?%s" % params)
+        assert r.status_code == 404
