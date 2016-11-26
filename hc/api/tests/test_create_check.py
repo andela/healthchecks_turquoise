ae8a66a80b2bd914c05bc2527893bde9a70a793a
Gilbert Gathara <gilbert.gathara@andela.com>
2016-11-26 12:57:36 +0300

    [Feature #133846503] Change handling invalid json parsed

--- hc/api/tests/test_create_check.py
+++ hc/api/tests/test_create_check.py
@@ -71,7 +71,7 @@ class CreateCheckTestCase(BaseTestCase):
         # r = {'status_code': 400, 'error': "could not parse request body"} ### This is just a placeholder variable
         r = self.client.post(self.URL, "string" ,content_type="application/json")
         self.assertEqual(r.status_code, 400)
-        self.assertEqual(r.error, "could not parse request body")
+        self.assertEqual(r['error'], "could not parse request body")
 
     def test_it_rejects_wrong_api_key(self):
         self.post({"api_key": "wrong"},
