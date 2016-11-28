115c8889d5dea0f8e5dde290d12ded75f2614d42
Gacheru <evans.gacheru@andela.com>
2016-11-28 12:07:14 +0300

    [Chore#133846507] fixed an assertion error on fuction that tests team access

--- hc/front/tests/test_add_check.py
+++ hc/front/tests/test_add_check.py
@@ -14,7 +14,6 @@ class AddCheckTestCase(BaseTestCase):
     ### Test that team access works
     def test_team_access_works(self):
         url = "/checks/add/"
-
         self.client.login(username="bob@example.org", password="password")
-        r = self.client.get(url)
-        self.assertContains(r, "Assign Checks to add", status_code=200)
+        r = self.client.post(url)
+        self.assertRedirects(r, "/checks/")
