4a75cb57cc8601a78a5957c2fa0948dfa6097741
Michael Kamau <michael.kamau@andela.com>
2016-11-29 15:42:38 +0300

    [Feature #133846503] Refactor to use self.assertEqual instead of assert

--- hc/api/tests/test_pause.py
+++ hc/api/tests/test_pause.py
@@ -14,10 +14,9 @@ class PauseTestCase(BaseTestCase):
                              HTTP_X_API_KEY="abc")
 
         ### Assert the expected status code and check's status
-        # >> DONE
         check.refresh_from_db()
-        assert r.status_code == 200
-        assert check.status == 'paused'
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(check.status, 'paused')
 
     def test_it_validates_ownership(self):
         check = Check(user=self.bob, status="up")
@@ -30,8 +29,7 @@ class PauseTestCase(BaseTestCase):
         self.assertEqual(r.status_code, 400)
 
         ### Test that it only allows post requests
-        # >> DONE
-        # https://docs.djangoproject.com/en/dev/topics/testing/advanced/
+        # Reference -->>> https://docs.djangoproject.com/en/dev/topics/testing/advanced/
         req_factory = RequestFactory()
         req = req_factory.post(url, "", content_type="application/json", HTTP_X_API_KEY="abc")
         self.assertEqual(req.method, 'POST')
